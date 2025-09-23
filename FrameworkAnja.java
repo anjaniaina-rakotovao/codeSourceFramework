1e5c758ee168865caeb2cabdd9b1d0e04cebad2c
anjaniaina-rakotovao <anjaniainarktv@gmail.com>
2025-09-23 22:26:45 +0300

    Sprint1 : Création frameworkAnja jar après test effectué

--- FrameworkAnja.java
+++ FrameworkAnja.java
@@ -2,6 +2,6 @@ package framework;
 
 public class FrameworkAnja {
     public static String testOfJar() {
-        return "Annyeong.. Ce salut c'est pour dire qu'il est bien rentré dans framework Anja!";
+        return "Annyeong.. Ce salut c'est pour dire qu'il est bien rentré dans framework Anja! Bienvenue";
     }
 }
