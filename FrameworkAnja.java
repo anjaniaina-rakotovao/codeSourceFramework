f731fe5ecb9fe3a9311a5dcce8647f6800140951
anjaniaina-rakotovao <anjaniainarktv@gmail.com>
2025-09-23 22:15:40 +0300

    first commit

---
+++ FrameworkAnja.java
@@ -0,0 +1,7 @@
+package framework;
+
+public class FrameworkAnja {
+    public static String testOfJar() {
+        return "Annyeong.. Ce salut c'est pour dire qu'il est bien rentré dans framework Anja!";
+    }
+}
